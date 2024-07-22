29582d7119867229a73465ef0532bf7b74b79933
Andrey <andrey.volk.1999@gmail.com>
2024-07-23 01:02:51 +1000

    add Solution OOP_2

--- src/Car.java
+++ src/Car.java
@@ -1,13 +1,16 @@
-public class Car {
+public class Car extends Transport {
 
-    public String modelName;
-    public int wheelsCount;
-
-    public void updateTyre() {
-        System.out.println("Меняем покрышку");
+    public Car(String modelName, int wheelsCount) {
+        super(modelName, wheelsCount);
     }
 
-    public void checkEngine() {
+    @Override
+    public void check(){
+        super.check();
+        checkEngine();
+    }
+
+    private void checkEngine() {
         System.out.println("Проверяем двигатель");
     }
 }
--- src/ServiceStation.java
+++ src/ServiceStation.java
@@ -1,23 +1,19 @@
 public class ServiceStation {
-    public void check(Car car, Bicycle bicycle, Truck truck) {
-        if (car != null) {
-            System.out.println("Обслуживаем " + car.modelName);
-            for (int i = 0; i < car.wheelsCount; i++) {
-                car.updateTyre();
-            }
-            car.checkEngine();
-        } else if (truck != null) {
-            System.out.println("Обслуживаем " + truck.modelName);
-            for (int i = 0; i < truck.wheelsCount; i++) {
-                truck.updateTyre();
-            }
-            truck.checkEngine();
-            truck.checkTrailer();
-        } else if (bicycle != null) {
-            System.out.println("Обслуживаем " + bicycle.modelName);
-            for (int i = 0; i < bicycle.wheelsCount; i++) {
-                bicycle.updateTyre();
-            }
-        }
+
+    public void checkTransport(Transport transport) {
+        System.out.printf("Обслуживается: [%s]\n", transport.getModelName());
+        transport.check();
+    }
+
+    public void check(Car car) {
+        car.check();
+    }
+
+    public void check(Bicycle bicycle) {
+        bicycle.check();
+    }
+
+    public void check(Truck truck) {
+        truck.check();
     }
 }
